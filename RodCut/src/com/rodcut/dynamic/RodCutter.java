1f19f4816284ae7af2930e1ca67dc2764eb37258
Chris Shoban <Chris.Shoban@gmail.com>
2013-04-08 21:33:17 -0700

    My first local commit

---
+++ RodCut/src/com/rodcut/dynamic/RodCutter.java
@@ -0,0 +1,13 @@
+package com.rodcut.dynamic;
+
+public class RodCutter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ RodCut/src/com/rodcut/greedy/RodCutter.java
@@ -0,0 +1,6 @@
+package com.rodcut.greedy;
+
+public class RodCutter {
+	
+	
+}
