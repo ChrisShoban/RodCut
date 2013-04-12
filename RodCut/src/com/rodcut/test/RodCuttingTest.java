1191f3036c431d38634307c36c93e219c0478b85
sampriya <monjury@gmail.com>
2013-04-11 23:28:56 -0700

    Updating test file

--- RodCut/src/com/rodcut/test/RodCuttingTest.java
+++ RodCut/src/com/rodcut/test/RodCuttingTest.java
@@ -9,10 +9,14 @@ public class RodCuttingTest {
 	public static void main(String[] args) {
 		int totalLength = 24;
 		ArrayList<Double> prices = new ArrayList<Double>();
+		prices.add(1.0);
+		prices.add(2.0);
+		prices.add(3.0);
+		prices.add(4.0);
 		RodCuttingModel model = new RodCuttingModel(totalLength, prices);
 		System.out.println("Greedy Algorithm Maximum Revenue : " + model.getGreedyAlgorithmMaximumRevenue());
-		System.out.println("Dynamic Programming Algorithm Maximum Revenue : " + model.getDynamicAlgorithmMaximumRevenue());
-		System.out.println("Branch And Bound Algorithm Maximum Revenue : " + model.getBranchAndBoundAlgorithmMaximumRevenue());
-		System.out.println("Divide And Conquer Algorithm Maximum Revenue : " + model.getDivideAndConquerAlgorithmMaximumRevenue());
+		// System.out.println("Dynamic Programming Algorithm Maximum Revenue : " + model.getDynamicAlgorithmMaximumRevenue());
+		// System.out.println("Branch And Bound Algorithm Maximum Revenue : " + model.getBranchAndBoundAlgorithmMaximumRevenue());
+		// System.out.println("Divide And Conquer Algorithm Maximum Revenue : " + model.getDivideAndConquerAlgorithmMaximumRevenue());
 	}
 }
