b384cb7b39bc0715ecda298fb57838beb938cf0c
Chris Shoban <Chris.Shoban@gmail.com>
2013-05-02 19:43:25 -0700

    Updated Dynamic Programming. Made profit table based on lengths and object ids

--- RodCut/src/com/rodcut/model/solutions/RodCuttingDynamicSolution.java
+++ RodCut/src/com/rodcut/model/solutions/RodCuttingDynamicSolution.java
@@ -28,29 +28,46 @@ public class RodCuttingDynamicSolution extends RodCuttingStrategy{
 		}
 	}
 	
-	@Override
 	public List<Rod> getMaximumRevenueRods(int totalLength, List<Rod> rodList) {
 		// Similar to Knapsack, we make a list of possibilities to choose from which is large
 		// than the total length of the rod (same as objects exceeding knapsack capacity)
 		createNodePossibilities(rodList);
 		
-		int capacity = totalLength;
+		//int capacity = totalLength;
 		int idCount = rodList.size();
-		// generate a 2d array (table) with total length and object id's
-		int[][] capacityById = new int[capacity][idCount];
+		// generate a 2d array (table) with total length and object id's set to the profit totals
+		double[][] profits = new double[totalLength][idCount];
 		
 		int currentLength = 0;
 		
 		// now we fill in the table to get the max revenue
-		for(int length = 0; length < capacity; length++) {
+		for(int length = 0; length < totalLength; length++) {
 			for(int id = 0; id < idCount; id++) {
 				currentLength = rodList.get(id).getLength();
 				if(currentLength <= length + 1) {
-					capacityById[length][id] = currentLength; // this is not right, but it will be similar to this maybe ...
+					profits[id][length] = maxProfit(id, length, rodList.get(id).getPrice(), possibilities, profits); // this is not right, but it will be similar to this maybe ...
 				}
 			}
 		}
 		
 		return null;
+	}
+	
+	public double maxProfit(int jthObject, int currentLength, double profitJ, List<Rod> rods, double[][] table) {
+		double tryToAddObject = 0;
+		double dontTryToAddObject = 0;
+		if(jthObject - 1 >= 0 && currentLength >= rods.get(jthObject).getLength()) {
+			tryToAddObject = table[jthObject - 1][currentLength - rods.get(jthObject).getLength()] + profitJ;
+		}
+		else if(jthObject - 1 >= 0){
+			dontTryToAddObject = table[jthObject - 1][currentLength];
+		}
+		
+		if(tryToAddObject > dontTryToAddObject) {
+			return tryToAddObject;
+		}
+		else {
+			return dontTryToAddObject;
+		}
 	}
 }
