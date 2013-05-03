cfcc9d3a66d0e1bd1b050ac8fbb29003e7403b1c
Chris Shoban <Chris.Shoban@gmail.com>
2013-05-03 14:33:24 -0700

    Progress on Dynamic Programming ...

--- RodCut/src/com/rodcut/model/solutions/RodCuttingDynamicSolution.java
+++ RodCut/src/com/rodcut/model/solutions/RodCuttingDynamicSolution.java
@@ -21,7 +21,7 @@ public class RodCuttingDynamicSolution extends RodCuttingStrategy{
 		for(int index = 0; index < totalLength; index++) {
 			currLen = rodList.get(index).getLength();
 			for(int eachRod = 0; eachRod < totalLength/currLen; eachRod++) {
-				possibilities.add(new Rod(rodList.get(index).getIndex(), 
+				possibilities.add(new Rod(rodList.get(index).getIndex() + eachRod, 
 										  rodList.get(index).getLength(), 
 										  rodList.get(index).getPrice()));
 			}
@@ -50,20 +50,19 @@ public class RodCuttingDynamicSolution extends RodCuttingStrategy{
 		createNodePossibilities(rodList);
 		
 		//int capacity = totalLength;
-		int idCount = rodList.size();
+		int idCount = possibilities.size();
 		// generate a 2d array (table) with total length and object id's set to the profit totals
-		double[][] profits = new double[totalLength][idCount];
-		
-		int currentLength = 0;
+		double[][] profits = new double[idCount][totalLength];
 		
 		// now we fill in the table to get the max revenue
-		for(int length = 0; length < totalLength; length++) {
-			for(int id = 0; id < idCount; id++) {
-					profits[id][length] = maxProfit(id, length, rodList.get(id).getPrice(), possibilities, profits); // this is not right, but it will be similar to this maybe ...
+		for(int id = 0; id < idCount; id++) {
+			for(int length = 0; length < totalLength; length++) {
+				double price = possibilities.get(id).getPrice();
+				profits[id][length] = maxProfit(id, length, price, possibilities, profits); // this is not right, but it will be similar to this maybe ...
 			}
 		}
 		
-		printTable(profits, totalLength, idCount);
+		printTable(profits, idCount, totalLength);
 		
 		return null;
 	}
@@ -71,7 +70,11 @@ public class RodCuttingDynamicSolution extends RodCuttingStrategy{
 	public double maxProfit(int jthObject, int currentLength, double profitJ, List<Rod> rods, double[][] table) {
 		double tryToAddObject = 0;
 		double dontTryToAddObject = 0;
-		if(jthObject - 1 >= 0 && currentLength >= rods.get(jthObject).getLength()) {
+		int currentRodLength = rods.get(jthObject).getLength();
+		if(currentLength >= currentRodLength) {
+			if(jthObject - 1 < 0) {
+				jthObject = 2;
+			}
 			tryToAddObject = table[jthObject - 1][currentLength - rods.get(jthObject).getLength()] + profitJ;
 		}
 		else if(jthObject - 1 >= 0){
